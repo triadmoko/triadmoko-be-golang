b3a42bbf5520f2486b4f4a433c35e3b876f0c4f4
triadmoko <triadmoko12@gmail.com>
2022-03-26 14:23:42 +0700

    update bug1

--- config/db.go
+++ config/db.go
@@ -3,13 +3,14 @@ package config
 import (
 	"fmt"
 	"os"
+	"triadmoko-be-golang/entity"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Database() *gorm.DB {
-	dsn := "host=" + os.Getenv("HOST") + " user=" + os.Getenv("USER") + " password=" + os.Getenv("PASSWORD") + " dbname=" + os.Getenv("DBNAME") + " port=" + os.Getenv("PORT") + " sslmode=enable TimeZone=Asia/Jakarta"
+	dsn := "host=" + os.Getenv("HOST") + " user=" + os.Getenv("USER") + " password=" + os.Getenv("PASSWORD") + " dbname=" + os.Getenv("DBNAME") + " port=" + os.Getenv("PORTDB") + " sslmode=enable TimeZone=Asia/Jakarta"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -17,6 +18,6 @@ func Database() *gorm.DB {
 	} else {
 		fmt.Println("Success connect database")
 	}
-	// db.AutoMigrate(&entity.User{})
+	db.AutoMigrate(&entity.User{})
 	return db
 }
