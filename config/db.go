bd3daec86c00aaaf5d89dbfd1ae5870b52965828
triadmoko <triadmoko12@gmail.com>
2022-03-26 09:39:13 +0700

    setup database

--- config/db.go
+++ config/db.go
@@ -1,1 +1,21 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Database() *gorm.DB {
+	dsn := "host=" + os.Getenv("HOST") + " user=" + os.Getenv("USER") + " password=" + os.Getenv("PASSWORD") + " dbname=" + os.Getenv("DBNAME") + " port=" + os.Getenv("PORT") + " sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Database not Connect")
+	} else {
+		fmt.Println("Success connect database")
+	}
+	return db
+}
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main()  {
-	
+import (
+	"log"
+	"triadmoko-be-golang/config"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// load .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	config.Database()
 }
