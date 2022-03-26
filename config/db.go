a020699d526f2aaa91cb1e918d4d9ad7a07845ac
triadmoko <triadmoko12@gmail.com>
2022-03-26 16:39:59 +0700

    change port

--- config/db.go
+++ config/db.go
@@ -9,7 +9,7 @@ import (
 )
 
 func Database() *gorm.DB {
-	dsn := "host=ec2-3-225-213-67.compute-1.amazonaws.com user=caivnfhfypngwn password=9a743a329c1b0855cb8504b551432c18861974de50ab9115102aa22b635d1226 dbname=dahc9ia5r6dt0m port=9920 sslmode=disable TimeZone=Asia/Jakarta"
+	dsn := "host=ec2-3-225-213-67.compute-1.amazonaws.com user=caivnfhfypngwn password=9a743a329c1b0855cb8504b551432c18861974de50ab9115102aa22b635d1226 dbname=dahc9ia5r6dt0m port=5432 sslmode=disable TimeZone=Asia/Jakarta"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
