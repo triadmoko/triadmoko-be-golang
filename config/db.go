176ce78fd9f421818a43d045a5911e50b0f63348
triadmoko <triadmoko12@gmail.com>
2022-03-26 16:36:25 +0700

    test

--- config/db.go
+++ config/db.go
@@ -2,16 +2,16 @@ package config
 
 import (
 	"fmt"
-	"os"
 	"triadmoko-be-golang/entity"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Database() *gorm.DB {
-	dsn := os.Getenv("USER") + ":" + os.Getenv("PASSWORD") + "@tcp(" + os.Getenv("HOST") + ":" + os.Getenv("PORTDB") + ")/" + os.Getenv("DBNAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := "host=ec2-3-225-213-67.compute-1.amazonaws.com user=caivnfhfypngwn password=9a743a329c1b0855cb8504b551432c18861974de50ab9115102aa22b635d1226 dbname=dahc9ia5r6dt0m port=9920 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		fmt.Println("Database not Connect")
 	} else {
