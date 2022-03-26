0ef9a69da2a52c4b874be68ed86c94b562840d44
triadmoko <triadmoko12@gmail.com>
2022-03-26 09:50:31 +0700

    test

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"triadmoko-be-golang/config"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -15,4 +16,12 @@ func main() {
 	}
 
 	config.Database()
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
